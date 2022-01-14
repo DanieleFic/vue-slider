c07af70489efe9ceb5e222176a81f06ee98f7429
DanieleFic <reb_ficus22@hotmail.it>
2022-01-14 14:52:21 +0100

    problemi di  css

--- js/myscript.js
+++ js/myscript.js
@@ -1,26 +1,3 @@
-/* ImmaginiArr = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]*/
 
 let app = new Vue({
     el: "#app",
@@ -56,7 +33,7 @@ let app = new Vue({
             if(indiceimmagine == this.corrente){
                 return "active";
             }
-            return "";
+            return "dnone";
         }
     }
 })
